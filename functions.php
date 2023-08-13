24ee0d32677456f1ffda783ff3d77660fbfdebfe
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-08-12 20:50:55 -0500

    Update home page, Bootstrap

--- functions.php
+++ functions.php
@@ -8,9 +8,8 @@
  */
 function utmsigep_enqueue_scripts()
 {
-  wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/popper.min.js', ['jquery'], '1.14.0', true);
-  wp_enqueue_script('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js', ['jquery'], '4.1.0', true);
-  wp_enqueue_script('fontawesome', 'https://use.fontawesome.com/releases/v5.0.12/js/all.js', [], '5.0.12', true);
+    wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.min.js', ['jquery'], '1.14.0', true);
+    wp_enqueue_script('bootstrap', ' 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js', ['jquery'], '5.3.1', true);
 }
 add_action('wp_enqueue_scripts', 'utmsigep_enqueue_scripts');
 
@@ -19,8 +18,9 @@ add_action('wp_enqueue_scripts', 'utmsigep_enqueue_scripts');
  */
 function utmsigep_enqueue_styles()
 {
-  wp_enqueue_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css', [], '4.1.0');
-  wp_enqueue_style('utmsigep', get_stylesheet_directory_uri() . '/assets/css/style.css', ['bootstrap']);
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css', [], '5.3.1');
+    wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.15.4/css/all.css', [], '5.15.4');
+    wp_enqueue_style('utmsigep', get_stylesheet_directory_uri() . '/assets/css/style.css', ['bootstrap']);
 }
 add_action('wp_enqueue_scripts', 'utmsigep_enqueue_styles');
 
@@ -29,20 +29,20 @@ add_action('wp_enqueue_scripts', 'utmsigep_enqueue_styles');
  */
 function utmsigep_theme_features()
 {
-  // Add theme support for Automatic Feed Links
-  add_theme_support('automatic-feed-links');
+    // Add theme support for Automatic Feed Links
+    add_theme_support('automatic-feed-links');
 
-  // Add theme support for Featured Images
-  add_theme_support('post-thumbnails');
+    // Add theme support for Featured Images
+    add_theme_support('post-thumbnails');
 
-  // Add theme support for HTML5 Semantic Markup
-  add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption']);
+    // Add theme support for HTML5 Semantic Markup
+    add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption']);
 
-  // Add theme support for document Title tag
-  add_theme_support('title-tag');
-  
-  // Add theme support for widgets
-  add_theme_support('widgets');
+    // Add theme support for document Title tag
+    add_theme_support('title-tag');
+
+    // Add theme support for widgets
+    add_theme_support('widgets');
 }
 add_action('after_setup_theme', 'utmsigep_theme_features');
 
@@ -51,25 +51,25 @@ add_action('after_setup_theme', 'utmsigep_theme_features');
  */
 function utmsigep_sidebars()
 {
-  register_sidebar([
-    'name' => 'Home Page Content',
-    'id' => 'home-page-content',
-    'class' => '',
-    'before_widget' => '<div id="%1$s" class="widget card %2$s">',
-    'after_widget' => '</div>' . PHP_EOL,
-    'before_title' => '<!-- Widget: ',
-    'after_title' => '-->'
-  ]);
+    register_sidebar([
+      'name' => 'Home Page Content',
+      'id' => 'home-page-content',
+      'class' => '',
+      'before_widget' => '<div id="%1$s" class="widget card %2$s">',
+      'after_widget' => '</div>' . PHP_EOL,
+      'before_title' => '<!-- Widget: ',
+      'after_title' => '-->'
+    ]);
 
-  register_sidebar([
-    'name' => 'Page Sidebar',
-    'id' => 'page-sidebar',
-    'class' => '',
-    'before_widget' => '<div id="%1$s" class="widget card mb-3 %2$s"><div class="card-body">',
-    'after_widget' => '</div></div>',
-    'before_title' => '<h2 class="card-title">',
-    'after_title' => '</h2>'
-  ]);
+    register_sidebar([
+      'name' => 'Page Sidebar',
+      'id' => 'page-sidebar',
+      'class' => '',
+      'before_widget' => '<div id="%1$s" class="widget card mb-3 %2$s"><div class="card-body">',
+      'after_widget' => '</div></div>',
+      'before_title' => '<h2 class="card-title">',
+      'after_title' => '</h2>'
+    ]);
 }
 add_action('after_setup_theme', 'utmsigep_sidebars');
 
@@ -78,11 +78,11 @@ add_action('after_setup_theme', 'utmsigep_sidebars');
  */
 function utmsigep_navigation_menus()
 {
-  $locations = [
-    'main_site_navigation' => __('Pages linked in header of the site.', 'utmsigep'),
-    'footer_site_navigation' => __('Pages linked in footer of the site.', 'utmsigep')
-  ];
-  register_nav_menus($locations);
+    $locations = [
+      'main_site_navigation' => __('Pages linked in header of the site.', 'utmsigep'),
+      'footer_site_navigation' => __('Pages linked in footer of the site.', 'utmsigep')
+    ];
+    register_nav_menus($locations);
 }
 add_action('init', 'utmsigep_navigation_menus');
 
--- home.php
+++ home.php
@@ -26,11 +26,14 @@
 
     <div class="container mt-5">
       <?php if (have_posts()):  while (have_posts()): the_post(); ?>
-        <?php the_title('<h2 class="mb-3">', '</h2>'); ?>
-        <?php the_date('F j, Y', '<p class="text-muted">', '</p>'); ?>
-        <?php the_content(); ?>
+        <div class="card">
+          <div class="card-body">
+            <?php the_title('<h2 class="mb-3">', '</h2>'); ?>
+            <?php the_date('F j, Y', '<p class="text-muted">', '</p>'); ?>
+            <?php the_content(); ?>
+          </div>
+        </div>
       <?php endwhile; ?>
-      <hr />
       <?php else: ?>
         <!-- <p>No posts found!</p> -->
       <?php endif; ?>
